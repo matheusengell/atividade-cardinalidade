fac7cb705d612b5a77332d27d9e94df884c25d33
matheus engel <matheusengelfl@gmail.com>
2026-03-25 16:57:56 -0300

    inicia o ClienteService

--- src/main/java/com/atividade/atividade2/service/ClienteService.java
+++ src/main/java/com/atividade/atividade2/service/ClienteService.java
@@ -1,9 +1,34 @@
 package com.atividade.atividade2.service;
 
+import com.atividade.atividade2.dto.cliente.ClienteRequestDto;
+import com.atividade.atividade2.dto.cliente.ClienteResponseDto;
+import com.atividade.atividade2.mapper.ClienteMapper;
+import com.atividade.atividade2.model.Cliente;
+import com.atividade.atividade2.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ClienteService {
+
+    private final ClienteRepository repository;
+    private final ClienteMapper mapper;
+
+    public ClienteResponseDto criarCliente(ClienteRequestDto requestDto){
+        Cliente cliente = mapper.paraEntidade(requestDto);
+        repository.save(cliente);
+        return mapper.paraDto(cliente);
+    }
+
+    public List<ClienteResponseDto> listarClientes(){
+        List<Cliente> clientes = repository.findAll();
+
+        return clientes.stream()
+                .map(mapper::paraDto)
+                .toList();
+    }
+
 }
