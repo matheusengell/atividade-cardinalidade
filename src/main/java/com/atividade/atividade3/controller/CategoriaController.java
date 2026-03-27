9ace415fb8e00e8113478b0c98edc78199222e13
matheus engel <matheusengelfl@gmail.com>
2026-03-27 20:28:36 -0300

    refactor(controller): padronizar retornos com ResponseEntity

--- src/main/java/com/atividade/atividade3/controller/CategoriaController.java
+++ src/main/java/com/atividade/atividade3/controller/CategoriaController.java
@@ -4,6 +4,8 @@ import com.atividade.atividade3.dto.categoria.CategoriaRequestDto;
 import com.atividade.atividade3.dto.categoria.CategoriaResponseDto;
 import com.atividade.atividade3.service.CategoriaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +16,14 @@ public class CategoriaController {
     private final CategoriaService service;
 
     @PostMapping
-    public CategoriaResponseDto criar(@RequestBody CategoriaRequestDto requestDto){
-        return service.criar(requestDto);
+    public ResponseEntity<CategoriaResponseDto> criar(@RequestBody CategoriaRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.criar(requestDto));
     }
 
     @GetMapping("/{id}")
-    public CategoriaResponseDto listarPorId(@PathVariable Long id){
-        return service.buscarPorId(id);
+    public ResponseEntity<CategoriaResponseDto> listarPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.buscarPorId(id));
     }
 }
--- src/main/java/com/atividade/atividade3/controller/ProdutoController.java
+++ src/main/java/com/atividade/atividade3/controller/ProdutoController.java
@@ -2,8 +2,11 @@ package com.atividade.atividade3.controller;
 
 import com.atividade.atividade3.dto.produto.ProdutoRequestDto;
 import com.atividade.atividade3.dto.produto.ProdutoResponseDto;
+import com.atividade.atividade3.model.Produto;
 import com.atividade.atividade3.service.ProdutoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +19,38 @@ public class ProdutoController {
     private final ProdutoService service;
 
     @PostMapping
-    public ProdutoResponseDto criar(@RequestBody ProdutoRequestDto requestDto){
-        return service.criar(requestDto);
+    public ResponseEntity<ProdutoResponseDto> criar(@RequestBody ProdutoRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.criar(requestDto));
     }
 
     @GetMapping
-    public List<ProdutoResponseDto> listarTodos(){
-        return service.listarTodos();
+    public ResponseEntity<List<ProdutoResponseDto>> listarTodos(){
+        List<ProdutoResponseDto> produtos = service.listarTodos();
+        return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("/categoria/id/{id}")
-    public List<ProdutoResponseDto> buscarCategoriaId(@PathVariable Long id){
-        return service.buscarPorCategoriaId(id);
+    public ResponseEntity<List<ProdutoResponseDto>> buscarCategoriaId(@PathVariable Long id){
+        List<ProdutoResponseDto> produtos = service.buscarPorCategoriaId(id);
+        return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("/nome/{nome}")
-    public List<ProdutoResponseDto> buscarPorNome(@PathVariable String nome){
-        return service.buscarPorNome(nome);
+    public ResponseEntity<List<ProdutoResponseDto>> buscarPorNome(@PathVariable String nome){
+        List<ProdutoResponseDto> produtos = service.buscarPorNome(nome);
+        return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("/categoria/nome/{nome}")
-    public List<ProdutoResponseDto> buscarPorNomeCategoria(@PathVariable String nome) {
-        return service.buscarPorNomeCategoria(nome);
+    public ResponseEntity<List<ProdutoResponseDto>> buscarPorNomeCategoria(@PathVariable String nome) {
+        List<ProdutoResponseDto> produtos = service.buscarPorNomeCategoria(nome);
+        return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("/{id}/{nome}")
-    public ProdutoResponseDto buscarPorIdAndNome(@PathVariable Long id ,@PathVariable String nome) {
-        return service.buscarPorIdENome(id, nome);
+    public ResponseEntity<ProdutoResponseDto> buscarPorIdAndNome(@PathVariable Long id ,@PathVariable String nome) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.buscarPorIdENome(id, nome));
     }
 }
