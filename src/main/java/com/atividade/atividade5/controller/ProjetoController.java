b44a21f37797ec9472b7ae0b67e1e4bc74f573b4
matheus engel <matheusengelfl@gmail.com>
2026-03-27 20:02:42 -0300

    refactor: aplica ResponseEntity no endpoint de tarefas e projeto

--- src/main/java/com/atividade/atividade5/controller/ProjetoController.java
+++ src/main/java/com/atividade/atividade5/controller/ProjetoController.java
@@ -2,8 +2,11 @@ package com.atividade.atividade5.controller;
 
 import com.atividade.atividade5.dto.projeto.ProjetoRequestDto;
 import com.atividade.atividade5.dto.projeto.ProjetoResponseDto;
+import com.atividade.atividade5.model.Projeto;
 import com.atividade.atividade5.service.ProjetoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +19,16 @@ public class ProjetoController {
     private final ProjetoService service;
 
     @PostMapping
-    public ProjetoResponseDto criar(@RequestBody ProjetoRequestDto requestDto){
-        return service.criar(requestDto);
+    public ResponseEntity<ProjetoResponseDto> criar(@RequestBody ProjetoRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.criar(requestDto));
     }
 
     @GetMapping
-    public List<ProjetoResponseDto> listarTodos(){
-        return service.listarTodos();
+    public ResponseEntity<List<ProjetoResponseDto>> listarTodos(){
+        List<ProjetoResponseDto> projetos = service.listarTodos();
+        return ResponseEntity.ok(projetos);
+
     }
 
 
--- src/main/java/com/atividade/atividade5/controller/TarefaController.java
+++ src/main/java/com/atividade/atividade5/controller/TarefaController.java
@@ -4,6 +4,8 @@ import com.atividade.atividade5.dto.tarefa.TarefaRequestDto;
 import com.atividade.atividade5.dto.tarefa.TarefaResponseDto;
 import com.atividade.atividade5.service.TarefaService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,28 +19,33 @@ public class TarefaController {
     private final TarefaService service;
 
     @PostMapping
-    public TarefaResponseDto criar(@RequestBody TarefaRequestDto requestDto){
-        return service.criar(requestDto);
+    public ResponseEntity<TarefaResponseDto> criar(@RequestBody TarefaRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.criar(requestDto));
     }
 
     @GetMapping
-    public List<TarefaResponseDto> listarTodas(){
-        return service.listarTodas();
+    public ResponseEntity<List<TarefaResponseDto>> listarTodas(){
+        List<TarefaResponseDto> tarefas = service.listarTodas();
+        return ResponseEntity.ok(tarefas);
     }
 
     @GetMapping("/projeto/{id}")
-    public List<TarefaResponseDto> listarPorProjetoId(@PathVariable Long id){
-        return service.listarPorProjetoId(id);
+    public ResponseEntity<List<TarefaResponseDto>> listarPorProjetoId(@PathVariable Long id){
+        List<TarefaResponseDto> tarefas = service.listarPorProjetoId(id);
+        return ResponseEntity.ok(tarefas);
     }
 
     @GetMapping("/{titulo}")
-    public List<TarefaResponseDto> listarPorTitulo(@PathVariable String titulo){
-        return service.listarPorTitulo(titulo);
+    public ResponseEntity<List<TarefaResponseDto>> listarPorTitulo(@PathVariable String titulo){
+        List<TarefaResponseDto> tarefas = service.listarPorTitulo(titulo);
+        return ResponseEntity.ok(tarefas);
     }
 
     @GetMapping("/filtro/{id}/{titulo}")
-    public TarefaResponseDto  listarPorIdAndTitulo(@PathVariable Long id,@PathVariable String titulo){
-        return service.listarPorIdAndTitulo(id, titulo);
+    public ResponseEntity<TarefaResponseDto> listarPorIdAndTitulo(@PathVariable Long id,@PathVariable String titulo){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.listarPorIdAndTitulo(id, titulo));
     }
 
 
